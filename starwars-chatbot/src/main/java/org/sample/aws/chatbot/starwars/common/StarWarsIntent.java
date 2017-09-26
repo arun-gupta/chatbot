c05934f5451154415fc2a22560fd876d340a0b6d
Arun Gupta <arun.gupta@gmail.com>
2017-09-26 14:15:29 -0700

    updating to the latest version of Lex bot

--- starwars-chatbot/src/main/java/org/sample/aws/chatbot/starwars/common/StarWarsIntent.java
+++ starwars-chatbot/src/main/java/org/sample/aws/chatbot/starwars/common/StarWarsIntent.java
@@ -1,11 +1,11 @@
 package org.sample.aws.chatbot.starwars.common;
 
 public final class StarWarsIntent {
-    public static final String MOVIE_INTENT = "MovieIntent";
-    public static final String PLANET_INTENT = "PlanetIntent";
-    public static final String LIGHTSABER_INTENT = "LightsaberIntent";
-    public static final String QUOTES_INTENT = "QuotesIntent";
-    public static final String FORCE_SENSITIVE_INTENT = "StarWarsForceSensitive";
-    public static final String FORCE_SIDE_INTENT = "StarWarsForceSide";
-    public static final String DIALOG_INTENT = "StarWarsDialog";
+    public static final String MOVIE_INTENT = "StarWarsMovieIntent";
+    public static final String PLANET_INTENT = "StarWarsPlanetIntent";
+    public static final String LIGHTSABER_INTENT = "StarWarsLightsaberIntent";
+    public static final String QUOTES_INTENT = "StarWarsQuotesIntent";
+    public static final String FORCE_SENSITIVE_INTENT = "StarWarsForceSensitiveIntent";
+    public static final String FORCE_SIDE_INTENT = "StarWarsForceSideIntent";
+    public static final String DIALOG_INTENT = "StarWarsAskQuestionIntent";
 }
